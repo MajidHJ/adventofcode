9e173047aa6d15aa602dd5b7fd9042fe16331196
MajidHJ <53868183+MajidHJ@users.noreply.github.com>
2025-12-07 16:09:02 +0330

    16-08-1404

--- day4/test.py
+++
@@ -1,7 +0,0 @@
-s1 = ['123','456','678','348']
-for i in range(len(s1[0])):
-    s = ''
-    for j in range(len(s1)):       
-        s += s1[j][i]
-    print(s)
-
--- day7/day7-2.py
+++ day7/day7-2.py
@@ -2,8 +2,8 @@ path = 'day7/input.txt'
 with open(path) as f:
     emiter_idx = f.readline().find('S')
     spliter_idx = []
-    beam_idx = set()
-    beam_idx.add(emiter_idx)
+    beam_idx = []
+    beam_idx.append(f'{emiter_idx}')
     print(emiter_idx)
     colision = 0
     for i in f:
@@ -11,7 +11,6 @@ with open(path) as f:
         spliter_idx = ([idx for idx,ch in enumerate(i) if ch == '^'])
         for s in spliter_idx:
             if s in beam_idx:
-                
                 beam_idx.remove(s)
                 beam_idx.add(s-1)
                 beam_idx.add(s+1)
