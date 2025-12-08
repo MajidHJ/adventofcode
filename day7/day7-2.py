91ec551022ffa1ca6c2026eb4bc04674a4bd7108
MajidHJ <53868183+MajidHJ@users.noreply.github.com>
2025-12-08 09:56:56 +0330

    sorted beam

--- day7/day7-2.py
+++ day7/day7-2.py
@@ -7,24 +7,37 @@ with open(spath) as f:
     all_beam_idx = []
     beam_idx = set()
     beam_idx.add(emiter_idx)
-    routes_count = 1
+    total_routes = 0
+    colision = 0
+    routes = [f'-{emiter_idx}']
     for i in f:
         # print(beam_idx)
         if not('^' in i) : continue
-        all_beam_idx.append(beam_idx)
+        bi = list(beam_idx)
+        bi.sort()
+        all_beam_idx.append(bi)
         spliter_idx = (([idx for idx,ch in enumerate(i) if ch == '^']))
         for s in spliter_idx:
             if s in beam_idx:
                 beam_idx.remove(s)
                 beam_idx.add(s-1)
                 beam_idx.add(s+1)
+                colision +=1
+                for r in routes.copy() :
+                    if r.endswith(f'-{s}'):
+                        routes.append(f'-{r}-{s-1}')
+                        routes.append(f'-{r}-{s+1}')
+                        routes.remove(r)
+        print(('-----',len(routes)))
         all_spliter_idx.append(spliter_idx)
 
 
-print(len(all_beam_idx))
-# print(len(all_spliter_idx))
 print(all_beam_idx)
-# print(all_spliter_idx[0])
+print(all_spliter_idx)
+print(len(routes))
 
-for i in range (len(all_beam_idx)):
+for i in all_beam_idx:
     ...
+
+print(total_routes)
+print(colision)
