4cf89f18616e3e9a8b237b4d3736e2c3e832bf14
MajidHJ <53868183+MajidHJ@users.noreply.github.com>
2025-12-09 19:00:08 +0330

    Update day9-2.py

---
+++ day9/day9-2.py
@@ -0,0 +1,30 @@
+import math
+
+spath = 'day9/sample.txt'
+ipath = 'day9/input.txt'
+path = ipath
+
+
+def area_vector(points):
+    n = len(points)
+    dist = []
+    for i in range(n):
+        for j in range(i+1,n):
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            a = math.fabs(x1-x2+1)*math.fabs(y1-y2+1)
+            dist.append((i,j,int(a)))
+
+    return dist
+
+
+with open(path) as f:
+    points = ([list(map(int,i.strip().split(','))) for i in f])
+    points.sort(key = lambda x: (x[0],x[1]))
+
+    print(points)
+
+
+area = area_vector(points)
+area.sort(key=lambda x: x[2])
+print(area[-1][2])
