8c6ea6a5a2b47705d2886929ab20bf3c4297086c
MajidHJ <53868183+MajidHJ@users.noreply.github.com>
2025-12-11 01:17:19 +0330

    as

--- day9/shaply_.py
+++ day9/shaply_.py
@@ -1,4 +1,4 @@
-from shapely.geometry import Polygon, box
+from shapely.geometry import Polygon, box, Point
 import math
 import matplotlib.pyplot as plt
 
@@ -9,8 +9,17 @@ from shapely.geometry import Polygon, box
 
 spath = 'day9/sample.txt'
 ipath = 'day9/input.txt'
-path = spath
-
+path = ipath
+# تابع کمکی برای رسم
+def plot_shape(shape, color='blue', alpha=0.5):
+    if shape.is_empty:
+        return
+    if hasattr(shape, 'geoms'):  # MultiPolygon یا GeometryCollection
+        for geom in shape.geoms:
+            plot_shape(geom, color, alpha)
+    else:
+        x, y = shape.exterior.xy
+        plt.fill(x, y, alpha=alpha, fc=color, ec='black')
 
 def area_vector(points):
     n = len(points)
@@ -25,45 +34,54 @@ def area_vector(points):
     return dist
 
 with open(path) as f:
-    shape_points = ([list(map(int,i.strip().split(','))) for i in f])    
+    shape_points = ([list(map(int,i.strip().split(','))) for i in f])
 poly = Polygon(shape_points)
-
 
 
 area = area_vector(shape_points)
 area.sort(key=lambda x: x[2],reverse=True)
+
+diffs = []
+
 for i in area:
     a,b,c = i
-    a = shape_points[a]
-    b = shape_points[b]
+    p1 = shape_points[a]
+    p2 = shape_points[b]
+    
+    x1, y1 = p1
+    x2, y2 = p2
 
-    x1, y1 = a
-    x2, y2 = b
     rect = box(min(x1, x2), min(y1, y2),
             max(x1, x2), max(y1, y2))
+    
+    p1 = Point((x1, y1))
+    p2 = Point((x1, y2))
+    p3 = Point((x2, y1))
+    p4 = Point((x2, y2))
 
-    is_inside = rect.covered_by(poly)
+    valid = poly.covers(p1) and poly.covers(p2) and poly.covers(p3) and poly.covers(p4)
+
+    if not valid : continue
+
+    if rect.difference(poly).area==0 :
+        diffs.append(f'{a}  {b} Area: {c}  Diff:  {rect.difference(poly).area}\n')
+
+    is_inside = poly.covers(rect)
 
     if is_inside : 
-        print(a,b,is_inside,f'Area: {c}')
+        print(poly.difference(rect).area)
+        print(rect.difference(poly).area)
+        print(rect.difference(poly).is_empty)
+        print(a,b,f'Area: {c}')
+        # رسم
+        plt.figure(figsize=(6,6))
+        plot_shape(poly, color='lightgreen', alpha=0.4)
+        plot_shape(rect, color='lightblue', alpha=0.6)
+
+        plt.axis('equal')
+        plt.show()
         break
 
+with open('Day9/x.txt','w') as f:
+    f.writelines(diffs)
 
-# تابع کمکی برای رسم
-def plot_shape(shape, color='blue', alpha=0.5):
-    if shape.is_empty:
-        return
-    if hasattr(shape, 'geoms'):  # MultiPolygon یا GeometryCollection
-        for geom in shape.geoms:
-            plot_shape(geom, color, alpha)
-    else:
-        x, y = shape.exterior.xy
-        plt.fill(x, y, alpha=alpha, fc=color, ec='black')
-
-# رسم
-plt.figure(figsize=(6,6))
-plot_shape(poly, color='lightgreen', alpha=0.4)
-plot_shape(rect, color='lightblue', alpha=0.6)
-
-plt.axis('equal')
-plt.show()
